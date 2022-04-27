567bd6d6e306b8f42b63c87c8e87b61f0d454686
Etienne <etienne.gavazzi@uit.no>
2022-04-27 13:07:43 +0000

    remove the experiments folders

--- b6/l7/testcase2/regen_par.m
+++
@@ -1,8 +0,0 @@
-%REGENERATION PARAMETERS
-FromDir = '../testcase1';
-Nprocs_new = 8; % Number of processes for the new run
-%END
-
-%REINITIALISATION
-rspecies = ; % reinitialise these species
-%END
